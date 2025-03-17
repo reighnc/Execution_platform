135483c0819674ae9da71f2542ac7c3a9d940215
reiner <74708863+reighnc@users.noreply.github.com>
2025-03-17 21:33:18 +0530

    Delete webapp directory

--- webapp/frontend/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import ensure_csrf_cookie
-
-
-@ensure_csrf_cookie
-def index(request):
-    return render(request, "index.html")
